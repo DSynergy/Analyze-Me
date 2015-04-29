fcb8e2c15f9fa140792d343f7f4ee452f9411e72
Dustin Gault <Dustin.Gault@gmail.com>
2015-04-28 21:17:02 -0600

    random background

--- app/controllers/oauth_controller.rb
+++
@@ -1,9 +0,0 @@
-class OauthController < ApplicationController
-
-  # def redirect
-  #   @graph = Koala::Facebook::API.new
-  #   session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
-  #   redirect_to session[:access_token] ? success_path : failure_path
-  # end
-
-end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
+    BACKGROUND = ['ankor', 'aspen', 'beats', 'berlin', 'budapest', 'buenos_aires', 'copper', 'cusco',
+   'cz', 'dcmetro', 'Denver', 'Haup', 'iguazu', 'jamaica', 'jamaica_country', 'Japan', 'Jelly_Fish',
+   'Kebler', 'rio', 'salt_flats', 'telluride', 'tokyo', 'uyuni']
+
   def randomized_background_image
-    images = ["assets/images/background_images/*"]
+    images = BACKGROUND.each {|image| ["#{image}.JPG"]}
     images[rand(images.size)]
   end
 

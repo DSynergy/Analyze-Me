9aaaca456b0184c1fbbcbe45f56bed387d812578
Dustin Gault <Dustin.Gault@gmail.com>
2015-05-23 15:16:08 -0600

    fixing images bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-    BACKGROUND = ['ankor', 'aspen', 'beats', 'berlin', 'budapest', 'buenos_aires', 'copper', 'cusco',
+    BACKGROUND = ['ankor', 'aspen', 'beats', 'berlin', 'buenos_aires', 'copper', 'cusco',
    'cz', 'dcmetro', 'Denver', 'Haup', 'iguazu', 'jamaica', 'jamaica_country', 'Japan', 'Jelly_Fish',
-   'Kebler', 'rio', 'salt_flats', 'telluride', 'istanbul', 'uyuni']
+   'Kebler', 'rio', 'salt_flats', 'telluride', 'uyuni']
 
   def randomized_background_image
     images = BACKGROUND.each {|image| ["#{image}.JPG"]}
